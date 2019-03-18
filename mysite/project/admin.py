b598a8249d2e510bc370f1093ff6e2fe6bcdc39f
Eliya <exaveryeliya20@gmail.com>
2019-03-18 09:10:16 +0300

    deleted file

--- mysite/project/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from project.models import Author
-from project.models import parallaxImg
-
-
-admin.site.register(Author)
-# Register your models here.
-admin.site.register(parallaxImg)
